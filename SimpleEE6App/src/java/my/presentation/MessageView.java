20d144c0cccd8e04c9fee776d8c25582120cc58d
tnes <torkel.nes@gmail.com>
2017-09-06 08:50:29 +0200

    SimpleEEApp

---
+++ SimpleEE6App/src/java/my/presentation/MessageView.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my.presentation;
+
+import boundary.MessageFacade;
+import entities.Message;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author TorkelNes
+ */
+@Named(value = "messageView")
+@RequestScoped
+public class MessageView {
+
+    @EJB
+    private MessageFacade messageFacade;
+
+    /**
+     * Creates a new instance of MessageView
+     */
+    public MessageView() {
+        this.message = new Message();
+    }
+    
+    // Creates a new field
+    private Message message;
+
+
+    // Calls getMessage to retrieve the message
+    public Message getMessage() {
+       return message;
+    }
+
+    // Returns the total number of messages
+    public int getNumberOfMessages(){
+       return messageFacade.findAll().size();
+    }
+
+    // Saves the message and then returns the string "theend"
+    public String postMessage(){
+       this.messageFacade.create(message);
+       return "theend";
+    }
+    
+}
